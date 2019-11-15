d8c17f78e1047955fbac6b0db35861bc0d18973f
ComIT-Ozzie <oz.neher+comit@gmail.com>
2019-11-14 20:13:59 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [firstNum, setFirstNum] = React.useState('');
+  let [secondNum, setSecondNum] = React.useState('');
+  let [operator, setOperator] = React.useState('');
+  let [result, setResult] = React.useState(null);
+
+  function placeNumber(num) {
+    if (operator === '') {
+      setFirstNum(firstNum + num);
+    } else {
+      setSecondNum(secondNum + num);
+    }
+  }
+
+  function calculate() {
+    switch (operator) {
+      case '+':
+        setResult(Number(firstNum) + Number(secondNum));
+        break;
+      case '-':
+        setResult(firstNum - secondNum);
+        break;
+      case '/':
+        setResult(firstNum / secondNum);
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="calculator">
+        <div className="display">
+          {firstNum === '' ? 0 : firstNum}
+          {operator}
+          {secondNum}
+        </div>
+
+        <div>
+          <button>Clear</button>
+          <button onClick={() => setOperator('/')}>/</button>
+        </div>
+        
+        <div>
+          <button onClick={() => placeNumber(7)}>7</button>
+          <button onClick={() => placeNumber(8)}>8</button>
+          <button onClick={() => placeNumber(9)}>9</button>
+          <button onClick={() => setOperator('-')}>-</button>
+        </div>
+
+        <div>
+          <button onClick={() => placeNumber(4)}>4</button>
+          <button onClick={() => placeNumber(5)}>5</button>
+          <button onClick={() => placeNumber(6)}>6</button>
+          <button onClick={() => setOperator('+')}>+</button>
+        </div>
+
+        <div>
+          <button onClick={() => placeNumber(1)}>1</button>
+          <button onClick={() => placeNumber(2)}>2</button>
+          <button onClick={() => placeNumber(3)}>3</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+
+      Result: {result}
     </div>
   );
 }
